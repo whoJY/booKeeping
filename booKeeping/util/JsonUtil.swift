953544dc1071aa15e4fa631e8e18e16980bbaece
胡锦宇 <2981363425@qq.com>
2019-03-29 16:51:37 +0800

    手动解析服务器传回来的json，对，就这么头铁

--- booKeeping/util/JsonUtil.swift
+++ booKeeping/util/JsonUtil.swift
@@ -17,7 +17,7 @@ class JsonUtil {
             let jsonEncoder = JSONEncoder()
             let jsonData = try jsonEncoder.encode(consArr)
             json = String(data: jsonData, encoding: String.Encoding.utf8) ?? ""
-
+            
         }catch
         {
             print("convert to json Str err")
@@ -55,5 +55,41 @@ class JsonUtil {
         }
         return String(data: data, encoding: String.Encoding.utf8)
     }
-   
+    
+    func parseJsonByhand(_ jsonStr:String)->[Consumption]{
+        //去除[、]、"、:和{
+
+      var  originStr = jsonStr.replacingOccurrences(of: "]", with: "")
+        originStr = originStr.replacingOccurrences(of: "{", with: "")
+        originStr = originStr.replacingOccurrences(of: "\"", with: "")
+    
+        
+        var cons = [Consumption]()
+        
+        let str = originStr.components(separatedBy: "},")
+        for subStr in str{
+            //最后一个数据有个}
+            let subStrStart  = subStr.replacingOccurrences(of: "}", with: "")
+            //去除id
+            let   subStr0 = subStrStart.replacingOccurrences(of: "[id:", with: "")
+            var subStr1 = subStr0.components(separatedBy: ",user_id:")
+            let id = subStr1[0] //得到id值
+            subStr1 = subStr1[1].components(separatedBy: ",name:")
+            let user_id = subStr1[0] //得到user_id
+            subStr1 = subStr1[1].components(separatedBy: ",kind:")
+            let name = subStr1[0]//得到name
+            subStr1 = subStr1[1].components(separatedBy: ",price:")
+            let kind = subStr1[0]//得到kind
+            subStr1 = subStr1[1].components(separatedBy: ",date:")
+            let price = subStr1[0]//得到price
+            let dateStr = subStr1[1]//得到dateStr
+            let consTemp : Consumption =   Consumption(id: id, user_id: user_id, name: name, kind: kind, price: Double(price)!, date: dateStr)
+            cons.append(consTemp)
+        }
+        
+        
+        return cons
+        
+    }
+    
 }
